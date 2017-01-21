ab79a31b338baae8ab9e2943d673998ed474ee5a
Aloving <Ilegend2011@gmail.com>
2017-01-21 04:45:18 +0300

    base actions

---
+++ server.js
@@ -0,0 +1,120 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let MongoClient = require('mongodb').MongoClient;
+let ObjectID = require('mongodb').ObjectID;
+
+let app = express();
+let db;
+
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/static'));
+
+app.use(function(req, res, next){         
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+app.get('/', function( req, res ){
+
+  let data;
+
+    db.collection('tasks').find().toArray(function(err, docs){
+      if(err){
+        data = 500;
+        res.send('<h2> Ошибка 500 </h2>')
+      }else{
+        data = docs;
+
+        res.render('index',{
+          data
+        });
+
+      }
+    });
+
+  
+});
+
+app.post('/add', function( req, res ){
+
+  [date, title, description, id] = 
+  [req.body.date, req.body.title, req.body.description];
+
+  if( !date || !title || !description ){
+    return res.status( 400 ).send('Неправильно введёднные данные');
+  }
+
+  let task = {
+    date,
+    title,
+    description
+  };
+
+  
+
+  db.collection('tasks').insert( task, function(err, result){
+    if(err){
+      console.log( err );
+      return res.sendStatus( 500 );
+    }else{
+
+      db.collection('tasks').find().toArray(function( err, docs ){
+        if( err ){
+          return res.sendStatus( 500 );
+        }
+
+        return res.send( docs );
+      });
+
+    }
+
+  });
+
+});
+
+app.post('/del', function( req, res ){
+
+  let idToDelete = req.body.id;
+
+  if( !idToDelete ){
+    return res.status( 400 ).send('Для удаление требуется ID');
+  }
+
+  db.collection('tasks').deleteOne({
+    _id: ObjectID(idToDelete)
+  }, function(err, result){
+    if( err ){
+      console.log( err );
+      return res.sendStatus( 500 );
+    }
+
+    if( result ){
+
+      db.collection('tasks').find().toArray(function( err, docs ){
+        if( err ){
+          return res.sendStatus( 500 );
+        }
+
+        return res.send( docs );
+      });
+
+
+    }
+    
+  });
+
+});
+
+
+MongoClient.connect('mongodb://localhost:27017/api', function(err, database){
+  if(err){
+    return console.log(err);
+  }
+  db = database;
+  app.listen(8000, function(){
+    console.log('api started');
+  });
+
+});
